b07a833ce4d02c437ac8e0e92fb580eb3433d15e
Nicolas Roos <nicolas.k.roos@gmail.com>
2015-07-19 17:07:39 +0200

    added getPersInit and getHeartrate funcs to import all the data

--- main.py
+++ main.py
@@ -2,17 +2,56 @@ import sys
 import getopt
 import csv
 
-def start_import():
-    print "I'm importing"
 
+def start_import():
     with open('data/init.csv', 'rU') as f:
         reader = csv.reader(f)
         rows = [row for row in reader if row]
         header = rows[0]
 
-        print header
-        for row in rows[1:]:
-            print row
+        for row in rows[1:2]: #The number two only for testing. Without it would iterate over all persons
+            person = {}
+            for i in range(len(header)):
+                person[header[i]] = row[i]
+
+            getPersInit(person)
+
+
+def getPersInit(person):
+    #print person
+    with open('data/' + person['ID'] + '/init.csv', 'rU') as f:
+        reader = csv.reader(f)
+        rows = [row for row in reader if row]
+        header = rows[0]
+
+        for row in rows [1:2]: #The number two only for testing. Without it would iterate over all persons
+            trainingunit = {}
+            for i in range(len(header)):
+                trainingunit[header[i]] = row[i]
+
+            getHeartrate(trainingunit,person['ID'])
+
+
+def getHeartrate(trainingunit,personId):
+    #print trainingunit
+    with open('data/' + personId + '/' + trainingunit['TU'] + '.csv', 'rU') as f:
+        reader = csv.reader(f)
+        rows = [row for row in reader if row]
+        header = rows[0]
+        heartrates = []
+        for row in rows [1:]: #The number two only for testing. Without it would iterate over all persons
+            heartrate = {}
+            tup = row[0], row[1]
+
+            #print tup
+            heartrates.extend(tup)
+
+        print heartrates
+
+
+
+
+
 
 
 # Main Functions
